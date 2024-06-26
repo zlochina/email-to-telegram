3890d4cda07d84573bd51e7609c9233c1a139d08
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-06-26 15:52:15 +0200

    Changed telegram message parsing to markdownv2

--- email_to_telegram.py
+++ email_to_telegram.py
@@ -24,7 +24,7 @@ def format_date(date_obj, format_string="%d/%m/%Y %H:%M"):
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    data = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "HTML"}
+    data = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "MarkdownV2"}
     response = requests.post(url, data=data)
     return response.json()
 
