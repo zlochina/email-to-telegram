6a83eacf62904de1c0d296aa3f9c772e0804a490
Vladyslav Zlochevskyi <zlochvla@fel.cvut.cz>
2024-06-26 15:02:28 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+
+# Email configuration
+EMAIL = 'your_email@example.com'
+PASSWORD = 'your_email_password'
+IMAP_SERVER = 'imap.example.com'
+IMAP_PORT = 993
+
+# Telegram configuration
+TELEGRAM_BOT_TOKEN = 'your_telegram_bot_token'
+TELEGRAM_CHAT_ID = 'your_telegram_chat_id'
+
